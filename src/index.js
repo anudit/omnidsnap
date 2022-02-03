2ba2d82c9ad3b46ab4deeed9d673d5eb38df94fc
Anudit Nagar <nagaranudit@gmail.com>
2022-02-03 13:06:45 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
-    case 'omnid_getTrustScoreData':
+    case 'omnid_getTrustScoreData': {
       const accounts = await wallet.request({ method: 'eth_requestAccounts' });
       const resp = await fetch(
         `https://theconvo.space/api/identity?address=${accounts[0]}&apikey=CONVO`,
       );
       const result = await resp.json();
       return result;
+    }
+
+    case 'omnid_default': {
+      // Just this case works
+      const accounts = await wallet.request({ method: 'eth_requestAccounts' });
+      return {
+        account: accounts[0],
+        scoreData: 50,
+      };
+    }
     default:
       throw new Error('Method not found.');
   }
